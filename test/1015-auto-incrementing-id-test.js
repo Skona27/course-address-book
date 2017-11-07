9d9d9600ac1c39bdd40b18f91b0c3cf7486aaf7e
BinarForge <contact@binarforge.com>
2017-11-07 21:38:52 +0000

    Added qUnit report document (html) and an example test around creating contacts and auto-incrementing contacts IDs.

---
+++ test/1015-auto-incrementing-id-test.js
@@ -0,0 +1,12 @@
+QUnit.test( "Test that contacts are created and added to the list.", function( assert ) {
+    assert.ok(contactList.length === 1, "Initially there is only one sample contact on the list." );
+
+    contactList.push(createContact(uniqueID, 'testname', 'testsurname', '123', 'test@test.com'))
+    assert.ok(contactList.length === 2, "Newly created contacted is appended to the list." );
+
+    QUnit.test( "Test that contacts are assigned with unique, incrementing ID.", function( assert ) {
+        assert.ok(contactList[0].id === 1, "Unique IDs start from 1." );
+        assert.ok(contactList[1].id === 2, "The next contact has got ID of 2 assigned to it." );
+    });
+});
+
